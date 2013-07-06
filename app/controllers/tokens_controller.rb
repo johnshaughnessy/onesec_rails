6b6b31dbbde84f1b8ace505b13d2b872060e617a
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-07-06 15:13:40 -0700

    what errors?

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -38,7 +38,7 @@ class TokensController  < ApplicationController
     @user=User.find_by_authentication_token(params[:id])
     if @user.nil?
       #logger.info(“Token not found.”)
-      render :status=>404, :json=>{:message=>”Invalid token.”}
+      render :status=>404, :json=>{:message=>"Invalid token."}
     else
       @user.reset_authentication_token!
       render :status=>200, :json=>{:token=>params[:id]}
