341b7d8a92a90842cfe22cd2c74b71e6745e675d
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-07-08 20:44:41 +0000

    changed just a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130706205551) do
+ActiveRecord::Schema.define(:version => 20130708203444) do
+
+  create_table "cakes", :force => true do |t|
+    t.string   "uid"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "video"
+    t.integer  "user_id"
+  end
 
   create_table "seconds", :force => true do |t|
     t.string   "date"
