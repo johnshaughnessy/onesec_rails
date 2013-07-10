43205b278480ddec63450ca07be6b90947f73623
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-07-09 18:20:35 -0700

    undoing...

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -11,8 +11,7 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  # storage :fog
-  include CarrierWaveDirect::Uploader
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
